9657d2aba0a2ce5c34d8c7c65792d7d6f186a112
TaigaSorimachi <taiga.sorimachi@gmail.com>
2026-03-11 17:02:26 +0900

    Add Vercel Speed Insights and Web Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import EngineerAssessment from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <EngineerAssessment />
+    <SpeedInsights />
+    <Analytics />
   </React.StrictMode>
 );
